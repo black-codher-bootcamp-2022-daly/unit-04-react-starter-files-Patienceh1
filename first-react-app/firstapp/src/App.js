5259a9804e8995f70e5d63ee507cc492a97c6f52
Patience Hodzi <patiencehodzi@gmail.com>
2022-11-23 18:30:18 +0000

    new-header-navbar

--- first-react-app/firstapp/src/App.js
+++ first-react-app/firstapp/src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './components/NavBar';
 
 function Header () {
   return <h1>Hey Patience</h1>
 }
+
+function Footer ({nickname}) {
+  return (
+    <div>
+      <h1>Goodbye {nickname}</h1>
+    </div>
+  )
+}
+
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header/>
+        <NavBar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,6 +35,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer nickname="Patience" />
     </div>
   );
 }
