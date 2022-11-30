4f312726dc0cebc2595aeeaa19d8c2cec7d3a62b
Patience Hodzi <patiencehodzi@gmail.com>
2022-11-30 18:53:31 +0000

    nests

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -7,10 +7,23 @@ function App() {
   //const books = data;
   const[books] = useState(data);
   return <ul>
+  <BookList>
   {books.map((book)=><Book item={book} key={book.id} />)}
   <Hook />
-  
+  </BookList>
   </ul>
+}
+
+
+function BookList (props) {
+return(
+    <div style={{
+      backgroundColor: "pink"
+    }}>
+      <h1>Patience's Book Case</h1>
+      <div className="BookList-Container">{props.children}</div>
+    </div>
+  )
 }
 
 export default App;
