222c3d8b97b9cae7d22fab4de7239b2ecf96fcdb
Patience Hodzi <patiencehodzi@gmail.com>
2022-11-30 16:15:29 +0000

    Hooks-Practice

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import Book from './components/Book';
+import Hook from './components/Hooks';
 import data from './models/books.json';
 
 function App() {
   //const books = data;
   const[books] = useState(data);
   return <ul>
-  {books.map((book)=><Book item={book}/>)}
+  {books.map((book)=><Book item={book} key={book.id} />)}
+  <Hook />
+  
   </ul>
 }
 
--- bookcase-app/src/components/Book.js
+++ bookcase-app/src/components/Book.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 const Book = (props) => {
   console.log(props)
-  return (
+
+  
+return (
     <ul>
-      <h2>{props.item.volumeInfo.title}</h2>;
+      <h2>{props.item.volumeInfo.title}</h2>
       <h3>{props.item.volumeInfo.authors}</h3>
+      <p>{props.item.volumeInfo.description}</p>
+      <h2>{props.item.volumeInfo.retailPrice}</h2>
+      <img>{props.item.volumeInfo.smallThumbnail}</img>
     </ul>
-  )
+    
+  ) 
 }
 
+
 export default Book;
+
