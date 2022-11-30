4523b6416afd843d8d4c31a2311b9f2e3a50f0f4
Patience Hodzi <patiencehodzi@gmail.com>
2022-11-30 19:48:24 +0000

    React-Nesting

--- nesting-and-events/src/index.js
+++ nesting-and-events/src/index.js
@@ -1,29 +1,41 @@
-import React, { Fragment } from 'react';
-import ReactDOM from 'react-dom/client';
-import TaskContainer from './components/TaskContainer';
-/*
+import React, { Fragment, useState } from "react";
+import ReactDOM from "react-dom/client";
+import TaskContainer from "./components/TaskContainer";
+import Task from "./components/Task";
+
 const taskList = [
-  {"id":"task1","title":"Buy Milk","isComplete":true},
-  {"id":"task2","title":"Read a book","isComplete":false},
-  {"id":"task3","title":"Early morning run","isComplete":false},
-  {"id":"task4","title":"Drink water","isComplete":false},
-  {"id":"task5","title":"Visit the puppy shelter","isComplete":true},
-  {"id":"task6","title":"Watch Bad Sisters","isComplete":false}
+  { id: "task1", title: "Buy Milk", isComplete: true },
+  { id: "task2", title: "Read a book", isComplete: false },
+  { id: "task3", title: "Early morning run", isComplete: false },
+  { id: "task4", title: "Drink water", isComplete: false },
+  { id: "task5", title: "Visit the puppy shelter", isComplete: true },
+  { id: "task6", title: "Watch Bad Sisters", isComplete: false },
 ];
-*/
 
 function App() {
-  
-   return (
-      <Fragment>
-        <h1 key="heading">Task List</h1>
-        <TaskContainer>
-          {/* Task: Nesting Components - Add you tasks here: */}
-        </TaskContainer>
-      </Fragment>
+  const [data, setData] = React.useState(taskList);
+  return (
+    <Fragment>
+      <h1 key="heading">Task List</h1>
+      <TaskContainer>
+        {taskList.map((props) => {
+          return (
+            <Task
+              key={props.id}
+              title={props.title}
+              isComplete={props.isComplete}
+            />
+          );
+        })}
+      </TaskContainer>
+    </Fragment>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
+/* <Task id="task1" title="Buy Milk" isComplete={true}/>
+          <Task id="task2" title="Read a book" isComplete={false}/>
+          <Task id="task4" title= "Drink water" isComplete={false}/>
+          <Task /> */
